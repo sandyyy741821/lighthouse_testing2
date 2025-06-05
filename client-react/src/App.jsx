419940184ffedd2632831a0935a8f65703ee5b96
fro29 <96358466+fro29@users.noreply.github.com>
2025-06-05 12:21:01 -0600

    Final change

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [users, setUsers] = useState([]); 
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/api/users')
+      .then(res => res.json())
+      .then(data => setUsers(data.users));
+  });
 
   return (
     <>
@@ -16,18 +23,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React with Vite + Python with React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h2>Users</h2>
+        
+          {users.map((user)=>(
+            <p key={user.id}>
+              {user.name}
+            </p>
+          ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- server-flask/main.py
+++ server-flask/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask-cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app) #enable CORS on all routes
@@ -17,7 +17,20 @@ def get_fruits():
 @app.route('/api/users')
 def get_users():
     return{
-        'users': ['Alice', 'Bob', 'Charlie']
+        'users': [
+            {
+                'id':1,
+                'name': 'Alice'
+            }, 
+            {
+                'id':2,
+                'name':'Bob'
+            },
+            {
+                'id':3,
+                'name':'Charlie'
+            }
+        ]
     }
 
 
